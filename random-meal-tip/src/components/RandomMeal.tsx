926004f4517d36e857e24c878f19be8da2fbc0bf
Sara Johnsson <sara86persson@hotmail.com>
2024-04-26 22:24:25 +0200

    fixed so that when you click the button a meal apear

--- random-meal-tip/src/components/RandomMeal.tsx
+++ random-meal-tip/src/components/RandomMeal.tsx
@@ -1,11 +1,31 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../main";
+import { MealTipStatus } from "../features/randommeal";
 
 
 function RandomMeal() {
 
+    const randomMeal = useSelector((state: RootState) => state.RandomMeal);
+
+    let content: string | null = null;
+
+    switch (randomMeal.status) {
+        case MealTipStatus.NORMAL:
+            content = 'Ready for a meal';
+            break;
+        case MealTipStatus.FETCHING:
+            content = 'Wating for meal';
+            break;
+        case MealTipStatus.SUCCESS:
+            content = randomMeal.fact;
+            break;
+        default:
+            content = 'No Meal available'
+    }
 
     return (
         <section>
-            <p>Olika mat förslag</p>
+            {content}
         </section>
     )
 }
